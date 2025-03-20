168cd62aadb589f067bc02364b32c7ea3ea2b817
hayward2007 <hayward2007@naver.com>
2025-03-20 16:24:36 +0900

    refactor: fixed all issues and features

--- defaults/vector.py
+++ defaults/vector.py
@@ -1,15 +1,5 @@
 import numpy as np
 
-class Vector:
-    def __init__(self, x, y, z):
-        self.vector = np.array([x, y, z])
-        self.vector = self.vector / np.linalg.norm(self.vector)
-    
-    def __cross__(self, other):
-        temp = np.cross(self.vector, other.vector)
-        return temp / np.linalg.norm(temp)
-    
-    def __unify__(self):
-        return self.vector / np.linalg.norm(self.vector)
-    
-    
+class Vector3D:
+    def unify(vector):
+        return vector / np.linalg.norm(vector)
